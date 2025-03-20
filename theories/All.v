63fb5592e47e1cb12bc732a5eaea73c32bdebe8f
Will Thomas <30wthomas@gmail.com>
2025-03-20 13:35:09 -0500

    Exporting specialized maps

--- theories/All.v
+++ theories/All.v
@@ -7,4 +7,13 @@ From Coq Require Export
 From RocqCandy Require Export Stringifiable Tactics.
 
 (* Exporting finite assoc list based maps *)
-From ExtLib Require Export Maps FMapAList.
+From ExtLib Require Export RelDec Maps FMapAList.
+
+Definition FMap K V `{RelDec K} := alist K V.
+Global Instance Map_FMap K V `{RelDec K} : Map K V (FMap K V).
+eapply Map_alist; eauto.
+Defined.
+
+Global Instance RelDec_EqDec T f `{EqDec T f} : RelDec f := {
+  rel_dec := fun x y => if equiv_dec x y then true else false
+}.
