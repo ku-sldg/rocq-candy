9c0a5d464af47dfa636f9ca72fbdfed24ce66029
Will Thomas <30wthomas@gmail.com>
2025-03-20 13:24:34 -0500

    More extlib exports

--- theories/All.v
+++ theories/All.v
@@ -5,3 +5,6 @@ From Coq Require Export
   Program.Basics 
   Setoid.
 From RocqCandy Require Export Stringifiable Tactics.
+
+(* Exporting finite assoc list based maps *)
+From ExtLib Require Export Maps FMapAList.
