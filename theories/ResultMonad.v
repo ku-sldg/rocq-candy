c5bdd60125fb354e84b67e94c28e4b84900ee4b9
Will Thomas <30wthomas@gmail.com>
2025-04-21 12:06:31 -0500

    Result unwrap notation

--- theories/ResultMonad.v
+++ theories/ResultMonad.v
@@ -5,7 +5,8 @@ Module Result.
   Arguments res {A B} _.
   Arguments err {A B} _.
 
-  Definition bind {A B} (x : t A B) (f : A -> t A B) : t A B :=
+  (* Monadic bind — general type *)
+  Definition bind {A B C} (x : t A B) (f : A -> t C B) : t C B :=
     match x with
     | res a => f a
     | err b => err b
@@ -14,17 +15,35 @@ Module Result.
   Definition ret {A B} (a : A) : t A B := res a.
   Definition raise {A B} (b : B) : t A B := err b.
 
+  (* Unwrap with fallback *)
+  Definition unwrap_or {A B} (x : t A B) (default : A) : A :=
+    match x with
+    | res a => a
+    | err _ => default
+    end.
+
+  (* Notation scope *)
   Module Notation.
+    Declare Scope result_scope.
+    Delimit Scope result_scope with result.
+
     Notation "x <- c1 ;; c2" := (bind c1 (fun x => c2))
-      (at level 61, c1 at next level, right associativity).
+      (at level 61, c1 at next level, right associativity) : result_scope.
 
     Notation "' pat <- c1 ;; c2" :=
       (bind c1 (fun x => match x with pat => c2 end))
-      (at level 61, pat pattern, c1 at next level, right associativity).
-    Notation "'ret' x" := (ret x) (at level 61, x at next level).
-    Notation "'raise' x" := (raise x) (at level 61, x at next level).
+      (at level 61, pat pattern, c1 at next level, right associativity) : result_scope.
+
+    Notation "'ret' x" := (ret x)
+      (at level 61, x at next level) : result_scope.
+
+    Notation "'raise' x" := (raise x)
+      (at level 61, x at next level) : result_scope.
+
+    Notation "x '<?>' y" := (unwrap_or x y)
+      (at level 61, y at next level) : result_scope.
 
   End Notation.
+
+  Export Notation.
 End Result.
-
-
