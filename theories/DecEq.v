a54f90996e7538ef11acb46454eb9f1c9297ff97
Will Thomas <30wthomas@gmail.com>
2025-06-10 14:58:41 -0500

    DecEq helper

--- theories/DecEq.v
+++ theories/DecEq.v
@@ -22,77 +22,58 @@ Global Instance DecEq_nat : DecEq nat := {
   dec_eq := Peano_dec.eq_nat_dec
 }.
 
+Ltac2 rec decdec_eq :=
+  fun () =>
+  match! goal with
+  | [ h1 : ?_t, h2 : ?_t, _h3 : DecEq ?_t |- _ ] =>
+    let h1 := Control.hyp h1 in
+    let h2 := Control.hyp h2 in
+    destruct (dec_eq $h1 $h2) > [
+      (* Left side, equality gets subst, eauto cleans trivial, recurse *)
+      subst_max; eauto; 
+      try (Control.enter decdec_eq)
+      | 
+      (* Right side, we must've had some contra thing *)
+      find_contra
+    ]
+  end.
+
+Ltac2 build_deq := fun () =>
+  ref (Build_DecEq _ _);
+  intros x; induction x; 
+  intros y; destruct y; 
+  eauto; try find_contra;
+  try (pp (&IHx &y); clear IHx;
+  match! goal with
+  | [ h : { _ } + { _ } |- _ ] =>
+    let h := Control.hyp h in
+    destruct $h > [
+      (* Left side, equality gets subst, eauto cleans trivial, recurse *)
+      subst_max; eauto;
+      Control.enter decdec_eq
+      | 
+      (* Right side, we must've had some contra thing *)
+      find_contra
+    ]
+  end);
+  Control.enter decdec_eq.
+
+Ltac2 Notation "build_deq" := build_deq ().
+
 Import ListNotations.
 
 Global Instance DecEq_list {A} `{HD : DecEq A} : DecEq (list A).
-ref (
-  Build_DecEq _
-  (fix F l1 l2 :=
-  match l1, l2 with
-  | [], [] => left eq_refl
-  | [], _ => right _
-  | _, [] => right _
-  | x1 :: xs1, x2 :: xs2 => 
-    match dec_eq x1 x2 with
-    | left Heq => 
-      match F xs1 xs2 with
-      | left Heq' => left _
-      | right Hneq => right _
-      end
-    | right Hneq => right _
-    end
-  end)
-); try congruence.
+build_deq.
 Defined.
 
 Global Instance DecEq_option {A} `{HD : DecEq A} : DecEq (option A).
-ref (
-  Build_DecEq _
-  (fun o1 o2 => 
-    match o1, o2 with
-    | None, None => left eq_refl
-    | Some x1, Some x2 => 
-      match dec_eq x1 x2 with
-      | left Heq => left _
-      | right Hneq => right _
-      end
-    | _, _ => right _
-    end
-  )
-); try congruence.
+build_deq.
 Defined.
 
 Global Instance DecEq_pair {A B} `{DA : DecEq A} `{DB : DecEq B} : DecEq (A * B).
-ref (
-  Build_DecEq _
-  (fun p1 p2 => 
-    let '(x1, y1) := p1 in
-    let '(x2, y2) := p2 in
-    match dec_eq x1 x2, dec_eq y1 y2 with
-    | left Heq_x, left Heq_y => left _
-    | _, _ => right _
-    end
-  )
-); try congruence.
+build_deq.
 Defined.
 
 Global Instance DecEq_sum {A B} `{DA : DecEq A} `{DB : DecEq B} : DecEq (A + B).
-ref (
-  Build_DecEq _
-  (fun p1 p2 => 
-    match p1, p2 with
-    | inl x1, inl x2 => 
-      match dec_eq x1 x2 with
-      | left HDA => left _
-      | _ => right _
-      end
-    | inr x1, inr x2 => 
-      match dec_eq x1 x2 with
-      | left HDA => left _
-      | _ => right _
-      end
-    | _, _ => right _
-    end
-  )
-); try congruence.
+build_deq.
 Defined.
