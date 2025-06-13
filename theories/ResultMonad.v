1a2d14f448bdb0677b3b2be3ceac8b58ebb345b7
Will Thomas <30wthomas@gmail.com>
2025-06-13 11:53:49 -0500

    Adding some result helper fns

--- theories/ResultMonad.v
+++ theories/ResultMonad.v
@@ -1,3 +1,6 @@
+From Stdlib Require Import List.
+From RocqCandy Require Import Tactics.
+
 Inductive Result (A B : Type) : Type :=
 | res : A -> Result A B
 | err : B -> Result A B.
@@ -31,3 +34,34 @@ Module Export ResultNotation.
   Hint Unfold bind : core.
   Hint Unfold unwrap_or : core.
 End ResultNotation.
+Export ResultNotation.
+
+Fixpoint result_map {A B E : Type} (f : A -> Result B E) (l : list A) 
+    : Result (list B) E :=
+  match l with
+  | nil => res nil
+  | (cons h t) =>
+      v <- f h;;
+      vs <- result_map f t;;
+      res (cons v vs)
+  end.
+
+Lemma result_map_spec : forall {X Y Z : Type} (l : list X) (f : X -> Result Y Z) x (resl : list Y),
+  In x l ->
+  result_map f l = res resl ->
+  (exists fx', (f x) = res fx' /\ In fx' resl).
+Proof.
+  induction l; simpl in *; intuition; eauto; ff u, a.
+  find_eapply_lem_hyp IHl; ff.
+Qed.
+
+Fixpoint result_fold {A B E : Type} (f : A -> B -> Result B E) 
+    (acc : B) (l : list A) 
+    : Result B E :=
+  match l with
+  | nil => res acc
+  | (cons h t) =>
+      acc' <- f h acc;;
+      result_fold f acc' t
+  end.
+
